420f546ad037b62f4964281662cf79c443a5bceb
Jane <jane@nhi.no>
2021-06-20 15:14:43 +0200

    Revert "Merge branch 'gh-pages' into main"
    This reverts commit 03121abb5b2b3e33415532f6986ebf5e22646645.

---
+++ src/views/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, Center } from "@chakra-ui/react";
+
+const About = () => {
+  return (
+    <Center bg="tomato" w="100vw" h="100vh">
+      <Text>Her kommer About..</Text>
+    </Center>
+  );
+};
+
+export default About;
